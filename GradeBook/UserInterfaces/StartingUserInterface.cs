4f64bfddf20ae98876322e8923920acb043049f1
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-03-23 17:30:20 +0100

    Update StartingUserInterfaces's HelpCommand method  Make the BaseGradeBook class abstract

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -59,11 +59,6 @@ namespace GradeBook.UserInterfaces
                     Console.WriteLine("{0} is not a supported type of gradebook, please try again", parts[2]);
                     return null;
             }
-
-
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
@@ -88,7 +83,7 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
