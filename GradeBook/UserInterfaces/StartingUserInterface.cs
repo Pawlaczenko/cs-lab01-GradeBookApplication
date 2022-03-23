5c9858bf4cdb46b1302ad98b439822b761ac2f3d
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-03-23 17:27:21 +0100

    Update StartingUserInterface's CreateCommand method

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -31,15 +31,36 @@ namespace GradeBook.UserInterfaces
                 Console.WriteLine("{0} was not recognized, please try again.", command);
         }
 
-        public static void CreateCommand(string command)
+        public static BaseGradeBook CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
-                return;
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                return null;
             }
+
             var name = parts[1];
+            var type = parts[2].ToLower();
+
+            switch (type)
+            {
+                case "standard":
+                    StandardGradeBook standardGradeBook = new StandardGradeBook(name);
+                    Console.WriteLine("Created gradebook {0}.", name);
+                    GradeBookUserInterface.CommandLoop(standardGradeBook);
+                    return standardGradeBook;
+                case "ranked":
+                    RankedGradeBook rankedGradeBook = new RankedGradeBook(name);
+                    Console.WriteLine("Created gradebook {0}.", name);
+                    GradeBookUserInterface.CommandLoop(rankedGradeBook);
+                    return rankedGradeBook;
+                default:
+                    Console.WriteLine("{0} is not a supported type of gradebook, please try again", parts[2]);
+                    return null;
+            }
+
+
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
