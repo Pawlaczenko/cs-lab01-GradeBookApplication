b2ce1cf94cc5dd1bb62140e69056e52ba04a0e45
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-03-23 16:33:32 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,26 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int no_of_students = Students.Count;
+            if (no_of_students < 5) throw new InvalidOperationException();
+
+            int stundenstPercentage = (int) Math.Round(0.2 * no_of_students);
+            List<double> allGrades = new List<double>();
+
+            foreach(Student s in Students)
+            {
+                allGrades.Add(s.AverageGrade);
+            }
+
+            int scoredHigher = allGrades.FindAll(el => el > averageGrade).Count;
+            char[] grades = { 'A', 'B', 'C', 'D', 'F' };
+
+            int index = scoredHigher / stundenstPercentage;
+            return grades[index];
+            
+        }
     }
 }
